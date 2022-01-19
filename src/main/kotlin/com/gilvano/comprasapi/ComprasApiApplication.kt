574191b684d9fba9313a9fff09bf51dd2524148c
gilvano <g.piontkoski@gmail.com>
2022-01-18 23:34:57 -0300

    Criar projeto

---
+++ src/main/kotlin/com/gilvano/comprasapi/ComprasApiApplication.kt
@@ -0,0 +1,11 @@
+package com.gilvano.comprasapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComprasApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComprasApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/gilvano/comprasapi/ComprasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gilvano.comprasapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComprasApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
