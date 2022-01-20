132fb919fb1b6ca49ebfbd45757073efbf8c03d6
gilvano <g.piontkoski@gmail.com>
2022-01-20 13:21:20 -0300

    Refatoração

--- src/main/kotlin/com/gilvano/comprasapi/component/CashbackCalculatorFactory.kt
+++ src/main/kotlin/com/gilvano/comprasapi/component/CashbackCalculatorFactory.kt
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Component
 
 @Component
 class CashbackCalculatorFactory {
-    private val cashbackFactorLow = 0.1
-    private val cashbackFactorMedium = 0.15
-    private val cashbackFactorHigh = 0.2
+    private val tenPercentCashback = 0.1
+    private val fifteenPercentCashback = 0.15
+    private val twentyPercentCashback = 0.2
 
     fun create(value: Double) = when {
-            value > 1500 -> CashbackCalculatorImpl(cashbackFactorHigh)
-            value > 1000 -> CashbackCalculatorImpl(cashbackFactorMedium)
-            else -> CashbackCalculatorImpl(cashbackFactorLow)
+            value > 1500 -> CashbackCalculatorImpl(twentyPercentCashback)
+            value > 1000 -> CashbackCalculatorImpl(fifteenPercentCashback)
+            else -> CashbackCalculatorImpl(tenPercentCashback)
         }
 }
