7a724da9c91ca50d6927f5e75863e3271bae8cbb
gilvano <g.piontkoski@gmail.com>
2022-01-21 09:58:37 -0300

    Refatoração

--- src/main/kotlin/com/gilvano/comprasapi/controller/request/PurchaseRequest.kt
+++ src/main/kotlin/com/gilvano/comprasapi/controller/request/PurchaseRequest.kt
@@ -1,10 +1,7 @@
 package com.gilvano.comprasapi.controller.request
 
-import org.hibernate.validator.constraints.Length
-import org.hibernate.validator.constraints.br.CPF
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
-import javax.validation.constraints.Digits
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Positive
 
@@ -21,10 +18,4 @@ data class PurchaseRequest(
     @NotEmpty(message = "Date is required")
     @DateTimeFormat(iso =  DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd")
     var date: LocalDate,
-
-//    @NotEmpty(message = "CPF is required")
-//    @field:CPF(message="CPF invalid")
-//    @field:Length(min = 11, max = 11, message = "CPF must have 11 characters")
-//    @field:Digits(integer = 11, fraction = 0, message = "CPF must have 11 digits")
-//    var cpf: String,
 )
