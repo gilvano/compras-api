48a9237b58bc634465704e31e285b05fed4c7e93
gilvano <g.piontkoski@gmail.com>
2022-01-22 16:31:32 -0300

    Ajuste

--- src/test/kotlin/com/gilvano/comprasapi/ComprasApiApplicationTests.kt
+++ src/test/kotlin/com/gilvano/comprasapi/ComprasApiApplicationTests.kt
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
+
 @SpringBootTest
 class ComprasApiApplicationTests {
 
-//	@Test
-//	fun contextLoads() {
-//	}
+	@Test
+	fun contextLoads() {
+	}
 
 }
