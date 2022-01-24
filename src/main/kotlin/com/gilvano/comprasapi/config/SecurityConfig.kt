2305aa3323c2712ee641859330ecf55811956722
gilvano <g.piontkoski@gmail.com>
2022-01-23 22:50:28 -0300

    Ajuste

--- src/main/kotlin/com/gilvano/comprasapi/config/SecurityConfig.kt
+++ src/main/kotlin/com/gilvano/comprasapi/config/SecurityConfig.kt
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -55,10 +54,6 @@ class SecurityConfig(
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder())
     }
-
-//    override fun configure(web: WebSecurity) {
-//        web.ignoring().antMatchers("/swagger-ui/**", "/javainuse-openapi/**")
-//    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
